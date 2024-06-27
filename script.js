34f76b7f2059e6bf36978ee94fce0377308a4663
Akshay <103642286+Akshay1495@users.noreply.github.com>
2024-06-27 15:25:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,53 +1,35 @@
-//your JS code here. If required.
-const tableBody=document.getElementById('output')
- 
-tableBody.innerHTML=`
-	<tr id="loading">
-		<td colspan="2">Loading...</td>
-	</tr>
-`
- 
-const randomTime=()=>{
-	return (Math.random()*2)+1
-}
-          
-const promise1=new Promise((res,rej)=>{
-	const time=randomTime()
-	setTimeout(()=>res({name:"Promise 1",time}),time*1000)
-})
-const promise2= new Promise((res,rej)=>{
-	const time=randomTime()
-	setTimeout(()=>res({name:"Promise 2",time}),time*1000)
-})
-const promise3= new Promise((res,rej)=>{
-	const time=randomTime()
-	setTimeout(()=>res({name:"Promise 3",time}),time*1000)
-})
+const output = document.getElementById("output");
+const btn = document.getElementById("download-images-button");
 
-const finalPromise=()=>{
-	 return Promise.all([promise1,promise2,promise3])
+const images = [
+  { url: "https://picsum.photos/id/237/200/300" },
+  { url: "https://picsum.photos/id/238/200/300" },
+  { url: "https://picsum.photos/id/239/200/300" },
+];
+
+// Function to download an image given its URL
+function downloadImage(image) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () => resolve(img);
+    img.onerror = () =>
+      reject(new Error(`Failed to load image's URL: ${image.url}`));
+    img.src = image.url;
+  });
 }
 
-finalPromise()
-	.then((values)=>{
-		const total=values.reduce((acc,curr)=>acc+Number(curr.time),0)		
-		
-		const tableRows=values.map((value)=>(
-			`
-			<tr>
-				<td>${value.name}</td>
-				<td>${value.time}</td>
-			</tr>
-		   `
-		)).join(' ')
+// Function to display downloaded images on the webpage
+function displayImages(images) {
+  images.forEach((img) => output.appendChild(img)); 
+}
 
-		tableBody.innerHTML=`${tableRows} 
-			<tr>
-				<td>Total</td>
-				<td>${total}</td>
-			</tr>
-		`
-		
-	})
-	.catch((err)=>console.error(err))
-	
+// Event listener for the download button
+btn.addEventListener("click", () => {
+  // Map each image URL to a downloadImage promise
+  const downloadPromises = images.map(downloadImage);
+
+  // Use Promise.all to download all images in parallel
+  Promise.all(downloadPromises)
+    .then(displayImages) // Display images on success
+    .catch((error) => console.error(error)); // Log error on failure
+});
